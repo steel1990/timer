4cfd61acaf405e48aa23d1e5184cc5513cfb0ad5
steel1990 <lbbsteel@gmail.com>
2016-06-07 16:08:13 +0800

    add timeout and fix some bug

--- test/ticker.js
+++ test/ticker.js
@@ -6,11 +6,15 @@ let ticker = new Ticker(2);
 let obj = {
     tick: deltaTime => console.log('ticker 1', deltaTime, new Date)
 };
-ticker.add(obj);
-setTimeout(() => {
-    ticker.remove(obj);
-    ticker.add({
-        tick: deltaTime => console.log('ticker 2', deltaTime, new Date)
-    });
-}, 5000);
+// ticker.add(obj);
+// setTimeout(() => {
+//     ticker.remove(obj);
+//     ticker.add({
+//         tick: deltaTime => console.log('ticker 2', deltaTime, new Date)
+//     });
+// }, 5000);
 ticker.start();
+
+ticker.timeout(1000, () => console.log('timout 1000:' + Date.now()));
+
+Ticker.timeout(2000, () => console.log('timout 2000:' + Date.now()));
